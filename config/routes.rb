ab42591e5c955bc3b45a8255c9bea27efc5855ab
PerfectJellyBean <perfectjelly00@gmail.com>
2022-04-09 19:19:31 -0400

    Heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
-    # root "articles#index"
+    root 'fallback#index'
     post '/signup', to: 'users#create'
     get '/me', to: 'users#show'
     post '/login', to: 'sessions#create'
